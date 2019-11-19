2b31d29d7304c082b2f60c28ab4f76673b84765d
Thiago Moreira <thiago.moreira@liferay.com>
2019-11-19 12:26:58 +0100

    add script to execute the upload for backups

---
+++ upload.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+number=$@
+
+curl -X POST https://backup-workshop$number-prd.lfr.cloud/backup/upload -H 'Content-Type: multipart/form-data' -F 'database=@backup/database.tgz' -F 'volume=@backup/volume.tgz' -u "workshop$number@liferay.com:devcon2019@MAINZ!?"
